0aed0c6c803a8362fcb3464ac17db557b128ac44
SnigdhaSunnam <132300286+SnigdhaSunnam@users.noreply.github.com>
2024-04-15 13:01:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
 
 const App = () => {
   const [width, setWidth] = useState("");
   const [height, setHeight] = useState("");
   const [backgroundColor, setBackgroundColor] = useState("");
+  const [boxes, setBoxes] = useState([]);
+
+  const handleCreateBox = () => {
+    const newBox = {
+      width: `${width}px`,
+      height: `${height}px`,
+      backgroundColor: backgroundColor
+    };
+
+    setBoxes([...boxes, newBox]);
+    setWidth("");
+    setHeight("");
+    setBackgroundColor("");
+  };
 
   return (
     <div id="main">
@@ -14,6 +28,8 @@ const App = () => {
         <input
           id="width"
           type="number"
+          value={width}
+          onChange={(e) => setWidth(e.target.value)}
         />
       </div>
       <div>
@@ -21,6 +37,8 @@ const App = () => {
         <input
           id="height"
           type="number"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
         />
       </div>
       <div>
@@ -28,13 +46,26 @@ const App = () => {
         <input
           id="backgroundColor"
           type="text"
+          value={backgroundColor}
+          onChange={(e) => setBackgroundColor(e.target.value)}
         />
       </div>
-      <button className='create-btn'>Create Box</button>
-      <div id="box-container"></div>
+      <button className='create-btn' onClick={handleCreateBox}>Create Box</button>
+      <div id="box-container">
+        {boxes.map((box, index) => (
+          <div
+            key={index}
+            style={{
+              width: box.width,
+              height: box.height,
+              backgroundColor: box.backgroundColor,
+              margin: '5px'
+            }}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 };
-
 
 export default App;
